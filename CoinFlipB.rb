6c95be0cc01bcd485d6912747e1791c157652422
SRGTxTwinkie <zanedreisbig@yahoo.com>
2019-10-22 12:32:45 -0400

    Brody Coin

--- CoinFlipB.rb
+++ CoinFlipB.rb
@@ -1,1 +1,30 @@
-puts "This is a test"
+$userpoints = 0
+$computerpoints = 0
+
+def Coinflip
+    print "Heads or Tails? "
+    guess = gets.chomp
+
+    if guess != "Heads" or guess != "Tails"
+    print "I said heads or tails buddy not whatever you want"
+    end
+
+    if rand(0..1) == 0
+        coin = "Heads"
+    else
+        coin = "Tails"
+    end
+
+    if coin.to_s == guess.to_s
+        print "The flip is " + coin.to_s + "! You win! \n"
+        $userpoints += 1
+    else
+        print "The flip is " + coin.to_s + "! You Lose! \n"
+        $computerpoints += 1
+    end
+
+    print "Player Points: " , $userpoints , "\n"
+    print "Computer Points: " , $computerpoints , "\n"
+    Coinflip()
+end
+Coinflip()
