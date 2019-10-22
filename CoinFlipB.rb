065e251058ee332dd93702be9e3da264d3b70e48
SRGTxTwinkie <zanedreisbig@yahoo.com>
2019-10-22 12:43:18 -0400

    It's fixed now

--- CoinFlipB.rb
+++ CoinFlipB.rb
@@ -2,11 +2,16 @@ $userpoints = 0
 $computerpoints = 0
 
 def Coinflip
-    print "Heads or Tails? "
-    guess = gets.chomp
+    print "Heads or Tails?: "
+    userGuess = gets.chomp
 
-    if guess != "Heads" or guess != "Tails"
-    print "I said heads or tails buddy not whatever you want"
+    if userGuess.upcase == "HEADS" or userGuess.upcase == "TAILS" ### I couldn't get the != to work, so I just remade it
+      ### This is blank because it does nothing, It's just a check to see
+      ### If the user chose one of the answers
+    else
+      print "I said Heads or Tails, not '#{userGuess}' \n" ### You can remove this and do whatever, that was just what I was checking with
+      print "Try is again \n"
+      Coinflip()
     end
 
     if rand(0..1) == 0
@@ -15,7 +20,7 @@ def Coinflip
         coin = "Tails"
     end
 
-    if coin.to_s == guess.to_s
+    if coin.to_s == userGuess
         print "The flip is " + coin.to_s + "! You win! \n"
         $userpoints += 1
     else
